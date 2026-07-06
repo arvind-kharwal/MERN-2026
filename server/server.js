9662431a82bf100a4e8b6f43d5872b4c553ffb91
Arvind Kharwal <karvind629@gmail.com>
2026-07-06 16:00:37 +0530

    Server code

--- server/server.js
+++ server/server.js
@@ -1,11 +1,38 @@
 import http from "http";
 import os from "os";
+
+const userdata = [
+    {
+        id: 101,
+        Name: "Arvind",
+        Email: "karvind08@gmail.com"
+    },
+    {
+        id: 102,
+        Name: "Yuvaan",
+        Email: "Yuvaan07@gmail.com"
+    }
+];
 const server = http.createServer((req,res)=>{
     const url = req.url;
+    console.log(url);
     const method = req.method;
     if(url=="/msg" && method == "GET"){
         res.write("Welcome User");
         res.end(); // It is used to get the output
+    }
+    else if(url=="/users" && method == "GET"){
+        res.end(JSON.stringify(userdata));
+    }
+
+    else if(url.startsWith("/user/") && method == "GET"){
+        const id = url.split("/")[2];
+        const user = userdata.find((u)=>u.id==id)
+        if(!user){
+            return res.end("User not found");
+        }
+        res.end(JSON.stringify(user));
+            
     }
     else if(url=="/system" && method == "GET"){
         const sysdata = {
