36fbe0c5ef54e8315552b23d1e7b0b0b54ff772e
Arvind Kharwal <karvind629@gmail.com>
2026-07-08 10:07:37 +0530

    8 July 2026 React JS

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function AdminDashboard() {
+const AdminDashboard = () => {
     return (
-        <div>AdminD ashboard</div>
+        <div>AdminDashboard</div>
     )
 }
 
