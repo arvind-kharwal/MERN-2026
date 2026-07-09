7b3bf68557f9ab9b7c64a9ad74a9e87477af2797
Arvind Kharwal <karvind629@gmail.com>
2026-07-09 12:42:55 +0530

    9 July 2026

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3003;
+app.use(cors());
+app.use(express.json());
 
+// testing whether server is running or not
 app.get("/",(req,res)=>{
     res.setHeader('Content-Type','text/html');
-    res.send('<h2 style=color:red> Welcome to Express Server</h2>')
+    res.send('<h2 style=color:red>Welcome to Express Server</h2>')
 })
+
+// app.get("/", (req, res) => {
+//     res.type('html');
+//     res.send('<h2 style="color:red">huyuyuyu</h2>');
+// });
+
+
+app.post("/register",(req,res)=>{
+    res.setHeader('Content-Type','application/json');
+    const {name,email,password} = req.body;
+    console.log(name+email+password);
+    res.send({"msg":name});
+})
+
 
 
 app.listen(port,()=>{
