0f43b1f8762a9ca7f61ca9e13c997b291cf065dd
Arvind Kharwal <karvind629@gmail.com>
2026-07-10 11:14:33 +0530

    10 July 2026

--- Middleware/server/server.js
+++ Middleware/server/server.js
@@ -2,9 +2,18 @@ import express from "express";
 import { m1 } from "./middleware/agemiddleware.js";
 const app = express();
 const port = 3005;
+// app.use(m1);
 app.use(m1);
 app.get("/msg",(req,res)=>{
     res.send("Welcome User");
+})
+
+app.get("/msg1",(req,res)=>{
+    res.send("Hi User");
+})
+
+app.get("/msg2",m1,(req,res)=>{
+    res.send("Hello User");
 })
 
 app.listen(port,()=>{
